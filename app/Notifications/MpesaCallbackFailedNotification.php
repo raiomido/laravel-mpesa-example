b8579d0d5abc9617bb3506b016864f65c0ad8280
Rai Omido <raiomidio@gmail.com>
2020-08-01 20:39:54 +0300

    Add acknowledgement response to Safaricom

---
+++ app/Notifications/MpesaCallbackFailedNotification.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Illuminate\Notifications\Notification;
+
+class MpesaCallbackFailedNotification extends Notification
+{
+    use Queueable;
+
+    private $message;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param string $message
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['slack'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the Slack representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return SlackMessage
+     */
+    public function toSlack($notifiable)
+    {
+        return (new SlackMessage())
+            ->content($this->message);
+    }
+}
--- config/misc/settings.php
+++ config/misc/settings.php
@@ -1,27 +1,4 @@
 <?php
 return [
-
-    /*
-    *--------------------------------------------------------------------------
-    * Application Date Format
-    *--------------------------------------------------------------------------
-    *
-    * Here you may specify the default date format for your application, which
-    * will be used with date and date-time functions.
-    *
-    */
-    'date_format' => 'd-m-Y',
-    'date_format_js' => 'dd-mm-yy',
-    'date_format_moment' => 'DD-MM-YYYY',
-    'time_format_moment' => 'HH:mm:ss',
-    'datetime_format_moment' => 'DD-MM-YYYY HH:mm:ss',
-
-    'recaptcha' => [
-        'site' => env('RECAPTCHA_SITE'),
-        'secret' => env('RECAPTCHA_SECRET'),
-    ],
-
-    'default_user_roles' => [
-        2
-    ],
+    'slack_webhook' => env("SLACK_WEB_HOOK")
 ];
