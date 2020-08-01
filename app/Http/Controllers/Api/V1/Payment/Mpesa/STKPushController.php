51a6d5457e327878fd315302032e90697ad8e2ed
Rai Omido <raiomidio@gmail.com>
2020-08-01 19:03:23 +0300

    Finish stk push

--- app/Http/Controllers/Api/V1/Payment/Mpesa/STKPushController.php
+++ app/Http/Controllers/Api/V1/Payment/Mpesa/STKPushController.php
@@ -3,44 +3,46 @@
 namespace App\Http\Controllers\Api\V1\Payment\Mpesa;
 
 use App\Http\Controllers\Controller;
-use App\STKPush;
+use App\Http\Requests\STKPushSimulateRequest;
+use App\Misc\Payment\Mpesa\Apis\STKPush;
 use Illuminate\Http\Request;
 
 class STKPushController extends Controller
 {
-    private $error;
+    public function simulate(STKPushSimulateRequest $request)
+    {
+        $env = config('misc.mpesa.stk_push', 'sandbox');
+
+        if ($config = config("misc.mpesa.stk_push.{$env}")) {
+            $init = (new STKPush())
+                ->setShortCode($config['short_code'])
+                ->setPassKey($config['pass_key'])
+                ->setAmount($request->amount)
+                ->setSenderPhone($request->sender_phone)
+                ->setPayerPhone($request->payer_phone)
+                ->setReceivingShortcode($config['short_code'])
+                ->setCallbackUrl(route('api.mpesa.stk-push.confirm', $config['confirmation_key']))
+                ->simulate($env);
+
+            if ($init->failed()) {
+                //do whatever
+            }
+            return $init->getResponse();
+        }
+        return response()->json([
+            'message' => 'Some config params are missing'
+        ], 400);
+    }
 
     public function confirm(Request $request)
     {
-        $env = config('misc.mpesa.env');
+        $env = config('misc.mpesa.stk_push', 'sandbox');
         $confirmation_key = config("misc.mpesa.stk_push.{$env}.confirmation_key");
 
-        if ($request->confirmation_key != $confirmation_key) {
-            $this->error = 'Confirmation key mismatch';
+        if ($request->confirmation_key == $confirmation_key) {
+            (new STKPush())->confirm($request);
+        } else {
+            // Send your self a notification
         }
-
-
-        //If no error, Change to success
-        if (!$this->error) {
-
-            $data = [
-                "result_desc"=>$request->Body->stkCallback->ResultDesc,
-                "result_code"=>$request->Body->stkCallback->ResultCode,
-                "merchant_request_id"=>$request->Body->stkCallback->MerchantRequestID,
-                "checkout_request_id"=>$request->Body->stkCallback->CheckoutRequestID,
-                "amount"=>$request->stkCallback->Body->CallbackMetadata->Item[0]->Value,
-                "mpesa_receipt_number"=>$request->Body->stkCallback->CallbackMetadata->Item[1]->Value,
-                "balance"=>$request->stkCallback->Body->CallbackMetadata->Item[2]->Value,
-                "b2c_utility_account_available_funds"=>$request->Body->stkCallback->CallbackMetadata->Item[3]->Value,
-                "transaction_date"=>$request->Body->stkCallback->CallbackMetadata->Item[4]->Value,
-                "phone_number"=>$request->Body->stkCallback->CallbackMetadata->Item[5]->Value
-            ];
-
-            STKPush::create($data);
-
-        }
-        return response()->json([
-            "error" => $this->error
-        ]);
     }
 }
